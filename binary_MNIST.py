904bacdd3ba9e9f11d611fc73251e9d981d9fc47
Gioele Bernardini <gioele.g@outlook.com>
2024-11-26 17:09:50 +0100

    Update: finish writing first binary implementation

--- binary_MNIST.py
+++ binary_MNIST.py
@@ -44,7 +44,7 @@ test_loader = torch.utils.data.DataLoader(
 )
 
 def binarize(tensor):
-  # Returns the sign of the vector (standard binarization)
+  # Returns the sign of the tensor (2d-matrix) (standard binarization)
   return tensor.sign()
 
 class BinarizeLinear(nn.Linear):
@@ -68,5 +68,98 @@ class BinarizeLinear(nn.Linear):
 
     return res
 
+class NeuralNetwork(nn.Module):
+  def __init__(self, input_size, hidden_size, num_classes):
+    super(NeuralNetwork, self).__init__()
 
+    self.l1 = nn.Linear(input_size, hidden_size)
+    self.bn1 = nn.BatchNorm1d(hidden_size)
+    self.htanh1 = nn.Hardtanh()
+
+    self.l2 = BinarizeLinear(hidden_size, 400)
+    self.bn2 = nn.BatchNorm1d(400)
+    self.htanh2 = nn.Hardtanh()
+
+    self.l3 = BinarizeLinear(400, 300)
+    self.bn3 = nn.BatchNorm1d(300)
+    self.htanh3 = nn.Hardtanh()
+
+    self.l4 = nn.Linear(300, num_classes)
+
+  def forward(self, x):
+    out = self.l1(x)
+    out = self.bn1(out)
+    out = self.htanh1(out)
+
+    out = self.l2(out)
+    out = self.bn2(out)
+    out = self.htanh2(out)
+
+    out = self.l3(out)
+    out = self.bn3(out)
+    out = self.htanh3(out)
+
+    out = self.l4(out)
+
+    return out
+
+# Model, loss function and optimizer
+model = NeuralNetwork(input_size, hidden_size, num_classes).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train the model
+# Does *not* train, but sets the mode for the model
+model.train()
+
+for epoch in range(num_epochs):
+  for i, (images, labels) in enumerate(train_loader):
+    images = images.reshape(-1, 28*28).to(device)
+    labels = labels.to(device)
+
+    # Forward pass
+    outputs = model(images)
+    loss = criterion(outputs, labels)
+
+    # Backward and optimize
+    optimizer.zero_grad()
+    loss.backward()
+    # optimizer.step() -> this leads to error
+
+    # Straight through estimator
+    # This is used to update the weights before binarizing them!
+    for p in list (model.parameters()):
+      if hasattr(p, 'org'):
+        p.data.copy_(p.org)
+
+    optimizer.step()
+
+    for p in list(model.parameters()):
+      if hasattr(p, 'org'):
+        p.org.copy_(p.data.clamp_(-1, 1))
+
+    if (i + 1) % 100 == 0:
+      print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+            .format(epoch+1, num_epochs, i+1, len(train_loader), loss.item()))
+
+# Finally, test the model
+model.eval()
+
+with torch.no_grad():
+  correct = 0
+  total = 0
+
+  for images, labels in test_loader:
+    images = images.reshape(-1, 28*28).to(device)
+    labels = labels.to(device)
+
+    outputs = model(images)
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted == labels).sum().item()
+
+  print('Accuracy of the network on the 10000 test images: {} %.'
+        .format(100 * correct / total))
 
