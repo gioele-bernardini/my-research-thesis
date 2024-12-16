cbd9b1ab59c3a54006bc5760b5ca39ad344564ec
Gioele Bernardini <gioele.g@outlook.com>
2024-12-16 19:04:57 +0100

    Update: 80% accuracy and translate download script for dataset

--- KWS_trainer.py
+++ KWS_trainer.py
@@ -12,9 +12,9 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyperparameters
 num_classes = None  # Will be determined based on the commands to load
-num_epochs = 50
-batch_size = 100
-learning_rate = 0.0001
+num_epochs = 100
+batch_size = 512
+learning_rate = 0.001
 
 # Dataset and commands directory
 dataset_dir = './speech-commands'
@@ -33,11 +33,71 @@ command_to_index = {command: idx for idx, command in enumerate(commands)}
 sample_rate = 16000  # Standard sampling rate for the dataset
 num_mel_bins = 64  # Number of Mel bands for the Mel-Spectrogram
 
-transform = torchaudio.transforms.MelSpectrogram(
-  sample_rate=sample_rate,
-  n_mels=num_mel_bins
+melkwargs = {"n_mels": 64, "n_fft": 400, "hop_length": 256, "center": False}
+transform = torchaudio.transforms.MFCC(
+ n_mfcc = 13,
+ sample_rate=sample_rate,
+ melkwargs = melkwargs,
+ log_mels = True,
 )
 
+def binarize(tensor):
+  # Returns the sign of the tensor (standard binarization)
+  return tensor.sign()
+
+class NeuralNetwork(nn.Module):
+  def __init__(self, input_size, hidden_size, num_classes):
+    super(NeuralNetwork, self).__init__()
+
+    # Replace nn.Linear with BinarizeLinear for all layers
+    self.l1 = BinarizeLinear(input_size, hidden_size)
+    self.bn1 = nn.BatchNorm1d(hidden_size)
+    self.htanh1 = nn.Hardtanh()
+
+    self.l2 = BinarizeLinear(hidden_size, 400)
+    self.bn2 = nn.BatchNorm1d(400)
+    self.htanh2 = nn.Hardtanh()
+
+    self.l3 = BinarizeLinear(400, 300)
+    self.bn3 = nn.BatchNorm1d(300)
+    self.htanh3 = nn.Hardtanh()
+
+    self.l4 = BinarizeLinear(300, num_classes)
+
+  def forward(self, x):
+    out = self.l1(x)
+    out = self.bn1(out)
+    out = self.htanh1(out)
+
+    out = self.l2(out)
+    out = self.bn2(out)
+    out = self.htanh2(out)
+
+    out = self.l3(out)
+    out = self.bn3(out)
+    out = self.htanh3(out)
+
+    out = self.l4(out)
+    return out
+
+class BinarizeLinear(nn.Linear):
+  def __init__(self, in_features, out_features, bias=True):
+    super(BinarizeLinear, self).__init__(in_features, out_features, bias)
+    # Save a copy of the original weights and biases for STE
+    self.weight.org = self.weight.data.clone()
+    self.bias.org = self.bias.data.clone()
+
+  def forward(self, input):
+    # Binarize input
+    input.data = binarize(input.data)
+
+    # Binarize weights and biases
+    self.weight.data = binarize(self.weight.org).to(self.weight.device)
+    if self.bias is not None:
+      self.bias.data = binarize(self.bias.org).to(self.bias.device)
+
+    output = nn.functional.linear(input, self.weight, self.bias)
+    return output
 # Binarization function with STE
 class BinarizeSTE(torch.autograd.Function):
   @staticmethod
@@ -91,14 +151,21 @@ class SpeechCommandsDataset(torch.utils.data.Dataset):
     # Apply transformation
     if self.transform:
       features = self.transform(waveform)
-      features = features.log2()  # Log-Mel Spectrogram
     else:
       features = waveform
 
     # Normalization
     features = (features - features.mean()) / (features.std() + 1e-5)
+    if torch.isnan(features.flatten()).sum():
+        assert 'oh no'
+        print('oh no 3 ')
+        x = torch.where(torch.isnan(features.flatten()))[0]
+        print(features[x[0]])
+        print(features.mean())
+        print(features.std())
+        exit()
 
-    return features, label
+    return features, label, filepath
 
 # Splitting dataset into training and test sets
 full_dataset = SpeechCommandsDataset(dataset_dir, commands, transform=transform)
@@ -121,8 +188,27 @@ test_loader = torch.utils.data.DataLoader(
 
 # Compute input size
 # The Mel-Spectrogram will have dimensions: (batch_size, channels, n_mels, time_steps)
-example_data, _ = next(iter(train_loader))
+example_data, _, _= next(iter(train_loader))
 input_size = example_data.shape[1] * example_data.shape[2] * example_data.shape[3]  # Correction here
+
+class BinarizeLinear(nn.Linear):
+  def __init__(self, in_features, out_features, bias=True):
+    super(BinarizeLinear, self).__init__(in_features, out_features, bias)
+    # Save a copy of the original weights and biases for STE
+    self.weight.org = self.weight.data.clone()
+    self.bias.org = self.bias.data.clone()
+
+  def forward(self, input):
+    # Binarize input
+    # input.data = binarize(input.data)
+
+    # Binarize weights and biases
+    self.weight.data = binarize(self.weight.org).to(self.weight.device)
+    if self.bias is not None:
+      self.bias.data = binarize(self.bias.org).to(self.bias.device)
+
+    output = nn.functional.linear(input, self.weight, self.bias)
+    return output
 
 # BinarizeLinearSTE class with STE
 class BinarizeLinearSTE(nn.Linear):
@@ -140,29 +226,73 @@ class BinarizeLinearSTE(nn.Linear):
     output = F.linear(input_bin, weight_bin, bias_bin)
     return output
 
-# Definition of the simplified neural network with additional fully connected layers
 class NeuralNetworkSimplified(nn.Module):
   def __init__(self, input_size, hidden_size1, hidden_size2, hidden_size3, num_classes):
     super(NeuralNetworkSimplified, self).__init__()
 
-    self.l1 = BinarizeLinearSTE(input_size, hidden_size1)
+    self.l1 = BinarizeLinear(input_size, hidden_size1)
     self.bn1 = nn.BatchNorm1d(hidden_size1)
     self.htanh1 = nn.Hardtanh()
     self.dropout1 = nn.Dropout(p=0.0)
 
-    self.l2 = BinarizeLinearSTE(hidden_size1, hidden_size2)
+    self.l2 = BinarizeLinear(hidden_size1, hidden_size2)
     self.bn2 = nn.BatchNorm1d(hidden_size2)
     self.htanh2 = nn.Hardtanh()
     self.dropout2 = nn.Dropout(p=0.0)
 
-    self.l3 = BinarizeLinearSTE(hidden_size2, hidden_size3)
+    self.l3 = BinarizeLinear(hidden_size2, hidden_size3)
+
     self.bn3 = nn.BatchNorm1d(hidden_size3)
+    self.htanh3 = nn.Hardtanh()
+    self.dropout3 = nn.Dropout(p=0.0)
+    self.l4 = nn.Linear(hidden_size3, num_classes)
+
+  def forward(self, x, y):
+    x = x.view(x.size(0), -1)
+    out = self.l1(x)
+    out = self.bn1(out)
+    out = self.htanh1(out)
+    out = self.dropout1(out)
+
+    out = self.l2(out)
+    out = self.bn2(out)
+    out = self.htanh2(out)
+    out = self.dropout2(out)
+
+    out = self.l3(out)
+    out = self.bn3(out)
+    out = self.htanh3(out)
+    out = self.dropout3(out)
+
+    out = self.l4(out)
+    return out
+# Definition of the simplified neural network with additional fully connected layers
+class NeuralNetworkSimplifiedSTE(nn.Module):
+  def __init__(self, input_size, hidden_size1, hidden_size2, hidden_size3, num_classes):
+    super(NeuralNetworkSimplifiedSTE, self).__init__()
+
+    self.l1 = BinarizeLinearSTE(input_size, hidden_size1)
+    # self.l1 = BinarizeLinear(input_size, hidden_size1)
+    # self.bn1 = nn.BatchNorm1d(hidden_size1)
+    self.htanh1 = nn.Hardtanh()
+    self.dropout1 = nn.Dropout(p=0.0)
+
+    self.l2 = BinarizeLinearSTE(hidden_size1, hidden_size2)
+    # self.l2 = BinarizeLinear(hidden_size1, hidden_size2)
+    # self.bn2 = nn.BatchNorm1d(hidden_size2)
+    self.htanh2 = nn.Hardtanh()
+    self.dropout2 = nn.Dropout(p=0.0)
+
+    # self.l3 = BinarizeLinear(hidden_size2, hidden_size3)
+    self.l3 = BinarizeLinearSTE(hidden_size2, hidden_size3)
+    # self.bn3 = nn.BatchNorm1d(hidden_size3)
     self.htanh3 = nn.Hardtanh()
     self.dropout3 = nn.Dropout(p=0.0)
 
     self.l4 = BinarizeLinearSTE(hidden_size3, num_classes)
+    # self.l4 = BinarizeLinear(hidden_size3, num_classes)
 
-  def forward(self, x):
+  def forward(self, x, y):
     x = x.view(x.size(0), -1)
     out = self.l1(x)
     out = self.bn1(out)
@@ -183,10 +313,11 @@ class NeuralNetworkSimplified(nn.Module):
     return out
 
 # Model hyperparameters
-hidden_size1 = 500  # You can modify this if necessary
-hidden_size2 = 300  # New layer
-hidden_size3 = 200  # New layer
+hidden_size1 = 512  # You can modify this if necessary
+hidden_size2 = 512  # New layer
+hidden_size3 = 512  # New layer
 model = NeuralNetworkSimplified(input_size, hidden_size1, hidden_size2, hidden_size3, num_classes).to(device)
+# model = NeuralNetworkSimplifiedSTE(input_size, hidden_size1, hidden_size2, hidden_size3, num_classes).to(device)
 
 # Loss function and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -196,20 +327,26 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 model.train()
 
 for epoch in range(num_epochs):
-  for i, (features, labels) in enumerate(train_loader):
+  correct, total = 0, 0
+  for i, (features, labels, fp) in enumerate(train_loader):
+    # print(fp)
     features = features.to(device)
     labels = labels.to(device)
 
     # Forward pass
-    outputs = model(features)
+    outputs = model(features, fp)
     loss = criterion(outputs, labels)
+
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted == labels).sum().item()
 
     # Backward and optimization
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
-  print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+  print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}, Acc: {100* correct/total:.2f}%')
 
 # Evaluating the model
 model.eval()
@@ -218,14 +355,16 @@ with torch.no_grad():
   correct = 0
   total = 0
 
-  for features, labels in test_loader:
-    features = features.to(device)
-    labels = labels.to(device)
+for features, labels, fp in test_loader:
+  features = features.to(device)
+  labels = labels.to(device)
 
-    outputs = model(features)
-    _, predicted = torch.max(outputs.data, 1)
-    total += labels.size(0)
-    correct += (predicted == labels).sum().item()
+  # Passa entrambi gli argomenti richiesti dal modello
+  outputs = model(features, fp)
+  _, predicted = torch.max(outputs.data, 1)
+  total += labels.size(0)
+  correct += (predicted == labels).sum().item()
 
-  print('Model accuracy on the test set: {:.2f}%'
-      .format(100 * correct / total))
+print('\nModel accuracy on the test set: {:.2f}%'
+    .format(100 * correct / total))
+
--- db_download.py
+++ db_download.py
@@ -4,32 +4,31 @@ import os
 import urllib.request
 import tarfile
 
-# URL del dataset di Google Speech Commands
+# URL of the dataset
 DATA_URL = "https://storage.googleapis.com/download.tensorflow.org/data/speech_commands_v0.02.tar.gz"
 
-# Nome del file compresso
 ARCHIVE_NAME = "speech_commands_v0.02.tar.gz"
-# Nome della cartella di estrazione
 OUTPUT_DIR = "speech-commands"
 
 def download_and_extract(url=DATA_URL, archive_name=ARCHIVE_NAME, output_dir=OUTPUT_DIR):
-  # Crea la cartella di output se non esiste
+  # Create the folder if it does not exist
   if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-  # Scarica il file solo se non esiste già
+  # Download the file only if it does not exist
   if not os.path.exists(archive_name):
-    print(f"Scaricamento del dataset da {url}...")
+    print(f"Downloading dataset from {url}...")
     urllib.request.urlretrieve(url, archive_name)
-    print("Download completato.")
+    print("Download completed.")
   else:
-    print("Il file è già presente, non è necessario scaricarlo.")
+    print("File already present, no need to download.")
 
-  # Estrai il contenuto all'interno della cartella di output
-  print("Estrazione del dataset...")
+  # Extract the content
+  print("Extracting dataset...")
   with tarfile.open(archive_name, "r:gz") as tar:
     tar.extractall(path=output_dir)
-  print(f"Estrazione completata nella cartella '{output_dir}'.")
+  print(f"Extraction completed in the '{output_dir}' directory.")
 
 if __name__ == "__main__":
   download_and_extract()
+
