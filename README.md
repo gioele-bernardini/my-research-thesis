cb7e245d31f4acfd197869bae341052362926b24
Gioele Bernardini <gioele.g@outlook.com>
2025-01-16 20:48:37 +0100

    Update: update script for download

---
+++ README.md
@@ -0,0 +1,8 @@
+python3 -m venv thesis_env
+source thesis_env/bin/activate
+
+pip install torch torchaudio numpy soundfile
+
+db_download.py
+K*
+generate
--- training/db_download.py
+++ training/db_download.py
@@ -11,24 +11,28 @@ ARCHIVE_NAME = "speech_commands_v0.02.tar.gz"
 OUTPUT_DIR = "speech-commands"
 
 def download_and_extract(url=DATA_URL, archive_name=ARCHIVE_NAME, output_dir=OUTPUT_DIR):
-  # Create the folder if it does not exist
-  if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    # Create the folder if it does not exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-  # Download the file only if it does not exist
-  if not os.path.exists(archive_name):
-    print(f"Downloading dataset from {url}...")
-    urllib.request.urlretrieve(url, archive_name)
-    print("Download completed.")
-  else:
-    print("File already present, no need to download.")
+    # Download the file only if it does not exist
+    if not os.path.exists(archive_name):
+        print(f"Downloading dataset from {url}...")
+        urllib.request.urlretrieve(url, archive_name)
+        print("Download completed.")
+    else:
+        print("File already present, no need to download.")
 
-  # Extract the content
-  print("Extracting dataset...")
-  with tarfile.open(archive_name, "r:gz") as tar:
-    tar.extractall(path=output_dir)
-  print(f"Extraction completed in the '{output_dir}' directory.")
+    # Extract the content
+    print("Extracting dataset...")
+    with tarfile.open(archive_name, "r:gz") as tar:
+        tar.extractall(path=output_dir)
+    print(f"Extraction completed in the '{output_dir}' directory.")
+
+    # Rimuovi l'archivio dopo l'estrazione
+    if os.path.exists(archive_name):
+        os.remove(archive_name)
+        print(f"Archive {archive_name} has been removed.")
 
 if __name__ == "__main__":
-  download_and_extract()
-
+    download_and_extract()
